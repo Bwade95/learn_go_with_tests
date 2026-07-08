571f5dd2c198450317c94b6a25823f8829f5c6ee
Brandon <bwade.95@hotmail.com>
2026-07-08 21:13:56 +0100

    added cases/test cases for other languages, refactored for readability

--- hello_world_test.go
+++ hello_world_test.go
@@ -5,14 +5,35 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		expected := "Hello, Brandon!"
-		actual := Hello("Brandon")
+		actual := Hello("Brandon", "English")
+
+		AssertEqual(t, expected, actual)
+	})
+
+	t.Run("say 'Hola, Mundo!' when no name is provided in Spanish", func(t *testing.T) {
+		expected := "Hola, Mundo!"
+		actual := Hello("", "Spanish")
+
+		AssertEqual(t, expected, actual)
+	})
+
+	t.Run("say 'Bonjour, Monde!' when no name is provided in French", func(t *testing.T) {
+		expected := "Bonjour, Monde!"
+		actual := Hello("", "French")
 
 		AssertEqual(t, expected, actual)
 	})
 
 	t.Run("say 'Hello, World!' when no name is provided", func(t *testing.T) {
 		expected := "Hello, World!"
-		actual := Hello("")
+		actual := Hello("", "")
+
+		AssertEqual(t, expected, actual)
+	})
+
+	t.Run("say 'こんにちは、Sekai!' when no name is provided in Japanese", func(t *testing.T) {
+		expected := "こんにちは、Sekai!"
+		actual := Hello("", "Japanese")
 
 		AssertEqual(t, expected, actual)
 	})
--- main.go
+++ main.go
@@ -2,13 +2,67 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	if name == "" {
-		name = "World"
+const (
+	english  = "English"
+	spanish  = "Spanish"
+	french   = "French"
+	japanese = "Japanese"
+)
+
+const (
+	englishHelloPrefix  = "Hello, "
+	spanishHelloPrefix  = "Hola, "
+	frenchHelloPrefix   = "Bonjour, "
+	japaneseHelloPrefix = "こんにちは、"
+)
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case japanese:
+		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return fmt.Sprintf("Hello, %s!", name)
+	return
+}
+
+func worldPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = "Mundo"
+	case french:
+		prefix = "Monde"
+	case japanese:
+		prefix = "Sekai"
+	default:
+		prefix = "World"
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	prefix := greetingPrefix(language)
+	world := worldPrefix(language)
+	
+	if name == "" {
+		name = world
+	}
+	return fmt.Sprintf("%s%s!", prefix, name)
+
 }
 
 func main() {
-	fmt.Println(Hello("Brandon"))
+	fmt.Println(Hello("Brandon", english))
+	fmt.Println(Hello("Brandon", spanish))
+	fmt.Println(Hello("Brandon", french))
+	fmt.Println(Hello("Brandon", japanese))
+	fmt.Println(Hello("", ""))
+	fmt.Println(Hello("", english))
+	fmt.Println(Hello("", spanish))
+	fmt.Println(Hello("", french))
+	fmt.Println(Hello("", japanese))
 }
